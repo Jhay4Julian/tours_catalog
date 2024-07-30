b8445c27dfc8cea331eb72c71e1ddd77375902bf
Julian Koripamo <jkoripamo@gmail.com>
2024-07-30 15:40:55 +0100

    initial commit

---
+++ lib/tours_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ToursPage extends StatefulWidget {
+  const ToursPage({super.key});
+
+  @override
+  State<ToursPage> createState() => _ToursPageState();
+}
+
+class _ToursPageState extends State<ToursPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
